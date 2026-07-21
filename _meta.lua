2b0b022092fdd8d2f3d39aa64cdb0c6d4e17cea4
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-21 15:49:06 +0200

    chore: bump version to 1.2.7

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "sudokux",
     fullname    = _("Sudoku X"),
     description = _("Sudoku with diagonal constraints"),
-    version     = "1.2.6",
+    version     = "1.2.7",
 }
