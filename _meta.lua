1e403d54d36af4cec154ec5d53185fc618edd6f5
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-04 12:52:04 +0200

    chore: bump version to 1.2.14

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     fullname    = _("Sudoku X"),
     description = _("Sudoku with diagonal constraints"),
-    version     = "1.2.13",
+    version     = "1.2.14",
 }
